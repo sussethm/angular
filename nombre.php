c84983bfa20e2f773288784f48f21c7366deb22c
sussethm <21045034@alumno.utc.edu.mx>
2023-10-04 19:38:37 -0500

    no supo hacerlo y lo hizo luuis xd pst no puse atencion

---
+++ nombre.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Denys Molina</title>
+    <style>
+        body {
+            background-image: url('imagen_de_fondo.jpg'); /* Cambia 'imagen_de_fondo.jpg' por la URL de tu imagen de fondo */
+            background-size: cover;
+            background-color: purple; /* Fondo morado */
+            color: white; /* Texto en color blanco */
+            font-size: 24px;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Establece la altura de la vista para centrar el contenido verticalmente */
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $nombre = "Denys Molina";
+    echo "<h1>$nombre</h1>";
+    ?>
+</body>
+</html>
